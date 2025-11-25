334b806bea00d8bbed4d01ffc361222442b17dda
vinograd <vinograd1020gm@gmail.com>
2025-11-25 18:07:06 +0300

    update 1

--- Lab5/Blue.cs
+++ Lab5/Blue.cs
@@ -7,11 +7,20 @@ namespace Lab5
     {
         public double[] Task1(int[,] matrix)
         {
-            double[] answer = null;
+            double[] answer = new double[matrix.GetLength(0)];
 
-            // code here
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int sum = 0;
 
-            // end
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] > 0)
+                        sum += matrix[i, j];
+                }
+                
+                answer[i] = sum / (double)matrix.GetLength(1);
+            }
 
             return answer;
         }
@@ -19,18 +28,44 @@ namespace Lab5
         {
             int[,] answer = null;
 
-            // code here
+            int maxElement = matrix[0, 0];
+            int imax = 0, jmax = 0;
 
-            // end
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] > maxElement)
+                    {
+                        maxElement = matrix[i, j];
+                        imax = i;
+                        jmax = j;
+                    }
+                }
+            }
+            
+            answer = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
 
+            for (int i = 0; i < answer.GetLength(0); i++)
+            {
+                if (i == imax)
+                    continue;
+                
+                for (int j = 0; j < answer.GetLength(0); j++)
+                {
+                    if (j == jmax)
+                        continue;
+                    
+                    answer[i, j] = matrix[i, j];
+                }
+            }
+            
             return answer;
         }
         public void Task3(int[,] matrix)
         {
 
-            // code here
-
-            // end
+            
 
         }
         public int[,] Task4(int[,] matrix)
